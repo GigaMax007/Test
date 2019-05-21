e9b85006ac5e865c8bffc2cc7137a9f5083d2293
Maksim Kishinskiy <kmaxk2006@mail.ru>
2019-05-22 03:25:01 +0500

    Изменен метод DivBigFile().  Исправлен Bug. Неправильно делил файл.

--- src/ru/gigamax/test/TestTask.java
+++ src/ru/gigamax/test/TestTask.java
@@ -6,13 +6,14 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- * Класс TestTask находит N (N=10) самых упоминаемых слов в большом текстовом файле и вывод результата в консоль (standart out).
+ * Класс TestTask находит N (N=10) самых упоминаемых слов в большом текстовом файле
+ * и вывод результата в консоль (standart out).
  * Результат  - отсортированный в обратном порядке,
  * список слов в нижнем регистре с количеством упоминаний каждого слова в тексте!!!
  *
  * @autor Максим Кишинский, kmaxk2006@mail.ru, т.+7(910)73-88-00-9, https://github.com/GigaMax007
  *
- * @version 2.1
+ * @version 2.2
  */
 public class TestTask {
     private static final String ENCODING = "UTF-8";
@@ -22,46 +23,68 @@ public class TestTask {
     public static Map<String, Long> resultMap = new HashMap<>(80_000);
 
     public static void main(String[] args) {
-        divBigFile(25);
-        for (int i = 0; i <= countFiles; i++) {
+        divBigFile("input.txt", 250_000.0);
+        for (int i = 1; i <= countFiles; i++) {
             String fileName = "mini" + i + ".tmp";
             createMap(fileName);
             resultMap = mergeTempMaps(tempMap);
         }
+        resultMap = mergeTempMaps(tempMap);
         System.out.println("resultMap.size() = " + resultMap.size());
         printSortedMap(resultMap, 10);
     }
-    // Деление большого файла на маленькие mini0...6.tmp
-    public static void divBigFile(int sizeMb) {
-        final String NAME = "input.txt";
-        BufferedWriter writer = null;
-        int count1Mb = 0;
-        long l = 0; // Cчетчик длин строк
+    // Деление большого файла на маленькие mini1...6.tmp
+    public static void divBigFile(String inputFile, double nol) {
+        System.out.println("Start divBigFile " + new Date().toString());
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(NAME), ENCODING));
-            for (String line1; (line1 = reader.readLine()) != null; ) {
-
-                if (l >= 1048576) {
-                    l = 0;
-                    count1Mb++;
-                }
-                if ((count1Mb % sizeMb == 0)&(l == 0)) {
-                    writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("mini" + (count1Mb / sizeMb) + ".tmp"), ENCODING));
-                    countFiles = count1Mb / sizeMb; // Итоговое число файлов равно (countFiles + 1)
-                }
-                writer.write(line1);
-                writer.newLine();
-                l += (long)line1.length();
+            // Чтение файла и подсчет количества строк и определение количества файлов, которые будут сгенерированы.
+            Scanner scanner = new Scanner(new File(inputFile));
+            int count = 0;
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                count++;
             }
-            writer.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println("Lines in the file: " + count);     // Выводит количество строк в исходном файле.
+            double temp = (count / nol);
+            int temp1 = (int) temp;
+            countFiles = 0;
+            if (temp1 == temp) countFiles = temp1;
+            else countFiles = temp1 + 1;
+            System.out.println("No. of files to be generated :" + countFiles); // Выводит количество файлов, которые будут сгенерированы.
+            // Разбиение файла на более мелкие
+            DataInputStream in = new DataInputStream(new FileInputStream(inputFile));
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            for (int j = 1; j <= countFiles; j++) {
+                // Файлы, которые будут созданы
+                BufferedWriter out = new BufferedWriter(new FileWriter("mini" + j + ".tmp"));
+                for (int i = 1; i <= nol; i++) {
+                    strLine = br.readLine();
+                    if (strLine != null) {
+                        out.write(strLine);
+                        if (i != nol) {
+                            out.newLine();
+                        }
+                    }
+                }
+                out.close();
+            }
+            in.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        // Вывод размера одного из разделенных файлов - mini1.tmp, если размер больше 85Мб
+        float fileSize = (new File("mini1.tmp")).length() / (1024.0f * 1024.0f);
+        if (fileSize > 85.0f) {
+            System.out.println("mini1.tmp = " + fileSize + " Mb");
+            System.out.println("Line size is about " + 1024 * 1024 * fileSize / nol + " Bytes");
+            System.out.println("Please, reduce the number of the lines (nol - parameter of divBigFile method) ");
         }
     }
     // Создание списка loadList и словаря tempMap
     public static void createMap(String mFileName) {
         BufferedReader reader;
-        loadList = new ArrayList<>(1_000_000);
+        loadList = new ArrayList<>(3_000_000);
         System.out.println("\nStart load List loadList " + new Date().toString());
 
         // Считываем файл name, например "mini0.tmp" и создаем список loadList
@@ -69,12 +92,14 @@ public class TestTask {
         try {
             reader = new BufferedReader(new InputStreamReader(new FileInputStream(mFileName), ENCODING));
             for (String line; (line = reader.readLine()) != null; ) {
-                StringTokenizer st = new StringTokenizer(line, " \t\r\f\\\'\"():,.&!|/«»@;*{}[]?\n");
+                StringTokenizer st = new StringTokenizer(line, " \t\r\f\\\'\"():,.&!|/«=»@;*{}[]?\n");
                 while (st.hasMoreTokens()) {
                     loadList.add(st.nextToken().toLowerCase());
                 }
             }
             reader.close();
+        } catch (OutOfMemoryError e) {
+            System.err.println(e.getMessage());
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -86,15 +111,14 @@ public class TestTask {
         // Удаление временного файла по завершении работы
         new File(mFileName).deleteOnExit();
     }
-    //Вывод отсортированного словаря в консоль
+    // Вывод отсортированного словаря в консоль
     public static void printSortedMap(Map<String, Long> map, int n){
         // Начинаем сортировку
         map.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).limit(n).forEach(System.out::println);
     }
     // Слияние временных словарей ArrayList<Map<String, Long>> allTempMap
     public static Map<String, Long> mergeTempMaps(Map<String, Long> allTempMap) {
-        /*     Используем метод merge, example:
-                    m2.forEach((k, v) -> m.merge(k, v, (v1, v2) -> v1 + v2));  */
+        // Используем метод merge, example: m2.forEach((k, v) -> m.merge(k, v, (v1, v2) -> v1 + v2));
         allTempMap.forEach((k, v) -> resultMap.merge(k, v, (v1, v2) -> v1 + v2));
         allTempMap.clear();
         return resultMap;
